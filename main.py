274a637e11edb3330008c3222b228c61cec72bb2
ammahmoudi <am.mahmoudi@outlook.com>
2025-01-26 20:32:09 +0330

    add chronos test

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from llms.time_llm import TimeLLM
 from utils.logger import setup_logging
 import logging
 
+from utils.result_saver import save_results_and_generate_plots
+
 FLAGS = flags.FLAGS
 flags.DEFINE_string('config_path', './configs/config_time_llm.gin', 'Path to config file.')
 flags.DEFINE_string('log_level', 'DEBUG', 'Logging level: DEBUG, INFO, WARNING, ERROR, or FATAL.')
@@ -115,12 +117,13 @@ def run(
                 data_settings['path_to_train_data'] = train_data_path
             if data_settings['path_to_test_data'].endswith('.csv'):
                 logging.debug("Loading test data from {}.".format(data_settings['path_to_test_data']))
-                test_data = data_loader.load_from_csv(data_settings['path_to_test_data'])
+                test_data = data_loader.load_from_csv(data_settings['path_to_test_data'],split='test')
+                test_input_features, test_labels = data_loader.split_dataframe_input_features_vs_labels(test_data)
 
             # prediction
-            llm_prediction,targets = llm.predict(
-                test_data=test_data,
-                data_loader=data_loader,
+            
+            llm_prediction = llm.predict(
+                test_data=test_input_features,
                 settings={
                     'prediction_length': llm_settings['prediction_length'],
                     'num_samples': llm_settings['num_samples'],
@@ -130,8 +133,10 @@ def run(
             )
             
             if llm_prediction is not None:
+
+                    # save_results_and_generate_plots(log_dir,predictions=llm_prediction,targets=test_labels.values,inputs=test_input_features)
                     # Evaluate metrics
-                    metric_results = llm.evaluate(llm_prediction, targets, llm_settings['eval_metrics'])
+                    metric_results = llm.evaluate(llm_prediction, test_labels.values, llm_settings['eval_metrics'])
                     logging.info(f"Metric results: {metric_results}")
 
         elif llm_settings['mode'] == 'training':
@@ -211,23 +216,29 @@ def run(
             # load checkpoint
             llm.load_model(llm_ckpt_path)
             # inference and evaluation
+           
             if data_settings['path_to_test_data'].endswith('.csv'):
+                logging.debug("Loading test data from {}.".format(data_settings['path_to_test_data']))
                 test_data = data_loader.load_from_csv(data_settings['path_to_test_data'],split='test')
+                test_input_features, test_labels = data_loader.split_dataframe_input_features_vs_labels(test_data)
 
 
-            llm_prediction,targets = llm.predict(
-                test_data=test_data,
-                data_loader=data_loader,
+            # prediction
+            
+            llm_prediction = llm.predict(
+                test_data=test_input_features,
                 settings={
                     'prediction_length': llm_settings['prediction_length'],
                     'num_samples': llm_settings['num_samples'],
                     'batch_size': llm_settings['prediction_batch_size'],
                     'auto_split': llm_settings['prediction_use_auto_split']
                 }
-            )
+            )     
             if llm_prediction is not None:
+
+                    # save_results_and_generate_plots(log_dir,predictions=llm_prediction,targets=test_labels.values,inputs=test_input_features)
                     # Evaluate metrics
-                    metric_results = llm.evaluate(llm_prediction, targets, llm_settings['eval_metrics'])
+                    metric_results = llm.evaluate(llm_prediction, test_labels.values, llm_settings['eval_metrics'])
                     logging.info(f"Metric results: {metric_results}")
 
     elif llm_settings['method'] == 'time_llm':
---
+++ scripts/chronos_test.sh
@@ -0,0 +1,1 @@
+python ./main.py --config_path ./configs/config_chronos_test.gin --log_level DEBUG
